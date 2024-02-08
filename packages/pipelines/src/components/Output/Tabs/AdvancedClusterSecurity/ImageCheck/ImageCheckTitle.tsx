695d8a441d3845d1c399d0fd52a53845dd78ac13
karthik <karthik.jk11@gmail.com>
2024-02-08 23:04:42 +0530

    fix(pipelines): replace static image url with actual image link

--- packages/pipelines/src/components/Output/Tabs/AdvancedClusterSecurity/ImageCheck/ImageCheckTitle.tsx
+++ packages/pipelines/src/components/Output/Tabs/AdvancedClusterSecurity/ImageCheck/ImageCheckTitle.tsx
@@ -1,6 +1,9 @@
-import { Text, TextContent, TextVariants } from '@patternfly/react-core';
+import { Text, TextContent, TextVariants, Button } from '@patternfly/react-core';
+import { useACSContext } from '../AdvancedClusterSecurityContext';
 
 const ImageCheckTitle = () => {
+  const { acsImageCheckResults } = useACSContext();
+
   return (
     <TextContent
       style={{
@@ -8,7 +11,21 @@ const ImageCheckTitle = () => {
       }}
     >
       <Text component={TextVariants.p}>
-        This task returns ACS vulnerability image check results for image: quay.io/repo
+        This task returns ACS vulnerability image check results for image:{' '}
+        <Button
+          variant="link"
+          isInline
+          component={(props) => (
+            <a
+              {...props}
+              href={`https://${acsImageCheckResults?.results?.[0]?.metadata?.additionalInfo?.name}`}
+              target="_blank"
+              rel="noreferrer"
+            />
+          )}
+        >
+          {acsImageCheckResults?.results?.[0]?.metadata?.additionalInfo?.name}
+        </Button>
       </Text>
     </TextContent>
   );
--- packages/pipelines/src/components/Output/Tabs/AdvancedClusterSecurity/ImageScan/ImageScanTitle.tsx
+++ packages/pipelines/src/components/Output/Tabs/AdvancedClusterSecurity/ImageScan/ImageScanTitle.tsx
@@ -1,6 +1,9 @@
-import { Text, TextContent, TextVariants } from '@patternfly/react-core';
+import { Button, Text, TextContent, TextVariants } from '@patternfly/react-core';
+import { useACSContext } from '../AdvancedClusterSecurityContext';
 
 const ImageScanTitle = () => {
+  const { acsImageCheckResults } = useACSContext();
+
   return (
     <TextContent
       style={{
@@ -8,7 +11,21 @@ const ImageScanTitle = () => {
       }}
     >
       <Text component={TextVariants.p}>
-        This task returns ACS vulnerability scan results for image: quay.io/repo
+        This task returns ACS vulnerability scan results for image:{' '}
+        <Button
+          variant="link"
+          isInline
+          component={(props) => (
+            <a
+              {...props}
+              href={`https://${acsImageCheckResults?.results?.[0]?.metadata?.additionalInfo?.name}`}
+              target="_blank"
+              rel="noreferrer"
+            />
+          )}
+        >
+          {acsImageCheckResults?.results?.[0]?.metadata?.additionalInfo?.name}
+        </Button>
       </Text>
     </TextContent>
   );
